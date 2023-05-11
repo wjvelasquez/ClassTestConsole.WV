1a1ada3252b2aa6f8c6a3a690d64d1b8f62e4db2
William Velasquez <williammaturin@gmail.com>
2023-05-11 00:16:50 -0400

    Update PersonExtensionMethods.cs
    Eliminados using innecesarios y agregada la verificacin de nulidad

--- ClassTest.App/Extensions/PersonExtensionMethods.cs
+++ ClassTest.App/Extensions/PersonExtensionMethods.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ClassTest.App.Extensions;
 static internal class PersonExtensionMethods
 {
     public static Person Upper(this Person person)
     {
-        person.Name = person.Name.ToUpper();
-        person.Lastname = person.Lastname.ToUpper();
+        person.Name = person.Name?.ToUpper();
+        person.Lastname = person.Lastname?.ToUpper();
 
         return person;
     }
